4fed80a1dc91b33f3915de0ddd0111154e13bedb
Iran Garcia <irangarciaj@gmail.com>
2024-09-02 18:47:34 -0300

    Add String and Operators snippets

---
+++ Operators.playground/Contents.swift
@@ -0,0 +1,67 @@
+// Operators
+
+// Assignment
+
+let b = 10
+
+var a = 5
+
+a = b // 10
+
+// Arithmetic
+
+1 + 2
+"Hello, " + "world!"
+
+5 - 3
+
+2 * 3
+
+10 / 2
+
+// Comparison
+
+1 == 1
+
+2 != 1
+
+2 > 1
+
+1 < 2
+
+1 >= 1
+
+2 <= 1
+
+
+// Nil coalescing
+
+var color: String?
+let red = "Red"
+
+color != nil ? color! : red
+
+// Range
+
+for index in 1...5 {
+    print(index) // 1 2 3 4 5
+}
+
+// Half-open range
+
+for index in 1..<5 {
+    print(index) // 1 2 3 4
+}
+
+// One-sided range
+
+let names = ["Anna", "Alex", "Brian", "Jack"]
+
+for name in names[2...] {
+    print (name) // Brian Jack
+}
+
+for name in names[...2] {
+    print (name) // Anna Alex Brian
+}
+
---
+++ Strings.playground/Contents.swift
@@ -0,0 +1,41 @@
+// String are value types
+// A new copy of the existing String is created and the new copy is passed or assigned (not the original version)
+
+let emptyString = ""
+
+let anotherEmptyString = String()
+
+let string = "Literal value"
+
+let multilineString = """
+This is the first line
+This is the second one
+"""
+
+// Interpolation
+
+let interpolation = "6 times 7 is \(6 * 7)."
+
+// Special characters
+
+let quote = "\"Stay hungry. Stay foolish\" - Steve Jobs"
+
+let blackHeart = "\u{2665}"
+
+// Methods
+
+var welcome = "Hello"
+
+welcome.insert("!", at: welcome.endIndex)
+welcome.insert(contentsOf: " there", at: welcome.index(before: welcome.endIndex))
+
+welcome.remove(at: welcome.index(before: welcome.endIndex))
+
+// String comparison
+
+let quotation = "We're equal"
+let sameQuotation = "We're equal"
+
+quotation.elementsEqual(sameQuotation)
+
+
